c07a236b8950b4e83954e8f142c439b74bf6a79b
Augusto de Nevreze <adenevreze@gmail.com>
2016-02-21 00:18:51 -0300

    Fixed home access via localhost:8000/

--- fschestakow/urls.py
+++ fschestakow/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^/', include('home.urls')),
+    url(r'^', include('home.urls')),
     url(r'^home/', include('home.urls')),
     url(r'^contacto/', include('contacto.urls')),
     url(r'^admin/', admin.site.urls),
